4fbb06f40717d4109152ff8a48304c582bc7e251
Milk-Cool <coder.tomsk@gmail.com>
2026-07-17 12:25:25 +0300

    that didn't quite work, what about this?

--- src/msbt.ts
+++ src/msbt.ts
@@ -2,5 +2,5 @@ import { spawnSync } from "child_process";
 
 export default function convert(path: string) {
     // TODO: Windows compatibility maybe?
-    if(spawnSync("wine", ["msbt/MSBTConverter.exe", path], { stdio: "pipe" }).status !== 0) throw new Error("MSBT conversion failed");
+    if(spawnSync("wine", ["msbt/MSBTConverter.exe", path], { stdio: "inherit" }).status !== 0) throw new Error("MSBT conversion failed");
 }
