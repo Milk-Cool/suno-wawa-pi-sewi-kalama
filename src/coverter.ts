f4d0d5cd53fd8d93d5964e2e89c6a5710bb6740d
Milk-Cool <coder.tomsk@gmail.com>
2026-07-17 12:40:30 +0300

    disable custom button names

--- src/coverter.ts
+++ src/coverter.ts
@@ -1,71 +1,73 @@
 import * as tokipona from "tokipona";
 
-const pronouncuations = {
-    // white
-    "": "a",
-    "": "pi",
-    "": "ekese",
-    "": "wa",
-    "": "mute",
-    "": "lili",
-    "": "tomo",
-    "": "mute",
-    "": "lili",
-    "": "sitelen",
-    "": "ele",
-    "": "ale",
-    "": "ele anpa",
-    "": "ale anpa",
-    "": "ele",
-    "": "ale",
-    "": "sewi",
-    "": "anpa",
-    "": "open",
-    "": "pini",
-    "": "sewi",
-    "": "anpa",
-    "": "open",
-    "": "pini",
+// can be customized in voicetext_ext_char_dict.msbt
 
-    // black
-    "": "a",
-    "": "pi",
-    "": "ekese",
-    "": "wa",
-    "": "mute",
-    "": "lili",
-    "": "tomo",
-    "": "mute",
-    "": "lili",
-    "": "sitelen",
-    "": "ele",
-    "": "ale",
-    "": "ele anpa",
-    "": "ale anpa",
-    "": "ele",
-    "": "ale",
-    "": "sewi",
-    "": "anpa",
-    "": "open",
-    "": "pini",
-    "": "sewi",
-    "": "anpa",
-    "": "open",
-    "": "pini",
+// const pronouncuations = {
+//     // white
+//     "": "a",
+//     "": "pi",
+//     "": "ekese",
+//     "": "wa",
+//     "": "mute",
+//     "": "lili",
+//     "": "tomo",
+//     "": "mute",
+//     "": "lili",
+//     "": "sitelen",
+//     "": "ele",
+//     "": "ale",
+//     "": "ele anpa",
+//     "": "ale anpa",
+//     "": "ele",
+//     "": "ale",
+//     "": "sewi",
+//     "": "anpa",
+//     "": "open",
+//     "": "pini",
+//     "": "sewi",
+//     "": "anpa",
+//     "": "open",
+//     "": "pini",
 
-    // joycon face buttons
-    "": "sewi",
-    "": "anpa",
-    "": "open",
-    "": "pini"
-};
+//     // black
+//     "": "a",
+//     "": "pi",
+//     "": "ekese",
+//     "": "wa",
+//     "": "mute",
+//     "": "lili",
+//     "": "tomo",
+//     "": "mute",
+//     "": "lili",
+//     "": "sitelen",
+//     "": "ele",
+//     "": "ale",
+//     "": "ele anpa",
+//     "": "ale anpa",
+//     "": "ele",
+//     "": "ale",
+//     "": "sewi",
+//     "": "anpa",
+//     "": "open",
+//     "": "pini",
+//     "": "sewi",
+//     "": "anpa",
+//     "": "open",
+//     "": "pini",
+
+//     // joycon face buttons
+//     "": "sewi",
+//     "": "anpa",
+//     "": "open",
+//     "": "pini"
+// };
 
 function convertOne(latin: string) {
     const split = latin.split(/([\r\n\ue0a0-\ue152])/gu);
     return split.map(x => "\r\n".includes(x)
         ? x
         : x.codePointAt(0)! >= 0xe0a0 && x.codePointAt(0)! <= 0xe152
-        ? x + pronouncuations[x as keyof typeof pronouncuations]
+        ? x/* + pronouncuations[x as keyof typeof pronouncuations]*/
         : tokipona.convert(x, "latin", "sitelen-pona/ucsur").replace(/\u{f199c}$/gu, "")
         + x.replaceAll(" ", "").replaceAll("j", "y").replaceAll(":",".").toLowerCase()).join("");
 }
