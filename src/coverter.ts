cf73f2ceda8c4f74ef74307e46342abe7c1fcb00
Milk-Cool <coder.tomsk@gmail.com>
2026-07-22 16:20:28 +0300

    mi pakala

--- src/coverter.ts
+++ src/coverter.ts
@@ -69,11 +69,12 @@ function convertOne(latin: string) {
         : x.codePointAt(0)! >= 0xe0a0 && x.codePointAt(0)! <= 0xe152
         ? x/* + pronouncuations[x as keyof typeof pronouncuations]*/
         : tokipona.convert(x, "latin", "sitelen-pona/ucsur").replace(/\u{f199c}$/gu, "")
-        + x.replace(/(?<=(^|\s|\.|\?|!)[jklmnpstw])a/g, "á")
+        + (x.includes("<") ? x
+        : x.replace(/(?<=(^|\s|\.|\?|!)[jklmnpstw])a/g, "á")
         .replace(/(?<=(^|\s|\.|\?|!)[jklmnpstw])i/g, "í")
         .replace(/(?<=(^|\s|\.|\?|!)[jklmnpstw])u/g, "ú")
         .replace(/(?<=(^|\s|\.|\?|!)[jklmnpstw])e/g, "é")
-        .replace(/(?<=(^|\s|\.|\?|!)[jklmnpstw])o/g, "ó")
+        .replace(/(?<=(^|\s|\.|\?|!)[jklmnpstw])o/g, "ó"))
         .replaceAll(" ", "").replaceAll("j", "y").replaceAll(":",".").toLowerCase()).join("");
 }
 export default function convert(latin: string) {
